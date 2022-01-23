d387718ae4446c7dfbd181420a5601699adce6ce
Sagar Tomar <sagartomar97@gmail.com>
2022-01-24 02:44:00 +0530

    Added sql queries for the answers

---
+++ week_1/queries.sql
@@ -0,0 +1,22 @@
+select count(*) from yellow_taxi_trips where date_part('month', "tpep_pickup_datetime")=1 and date_part('day', "tpep_pickup_datetime")=15
+
+select date_part('day', "tpep_pickup_datetime"), max("tip_amount") as tip from yellow_taxi_trips
+where date_part('month', "tpep_pickup_datetime")=1
+group by date_part('day', "tpep_pickup_datetime")
+order by tip desc
+
+select
+	D."Zone",
+	count(*) as total
+from
+	yellow_taxi_trips as trips
+join zones as PU
+	on trips."PULocationID"=PU."LocationID"
+join zones as D
+	on trips."DOLocationID"=D."LocationID"
+where
+	PU."Zone"='Central Park' and
+	date_part('day', trips."tpep_pickup_datetime")=14 and
+	date_part('month', trips."tpep_pickup_datetime")=1
+group by D."Zone"
+order by total desc
